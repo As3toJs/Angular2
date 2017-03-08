556fdf1a4d3c27297e0ac16775a1b1b5d2e13c7e
piyush <piyush@edulab.dk>
2017-03-08 10:36:00 +0100

    var name change

--- test/tt.ts
+++ test/tt.ts
@@ -1,20 +1,6 @@
+var randomNumber = getRandom(2,9);
 
-console.log('test-app-1', Math.floor(Math.random()*50));
-
-
-console.log(number.value);
-
-var random = getRandom(2,9);
-
-console.log('Your Random number:', random);
-
-document.getElementById('btn').onclick = function(){
-	console.log('btn clicked');
-}
-
-function onClick(){
-	console.log('btn clicked');
-}
+console.log('Your Random number:', randomNumber);
 
 function getRandom(min, max){
 	return Math.floor((Math.random()* (max-min)) + min);
